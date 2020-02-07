8817c29f34a9734439248169242680594ef78c25
AngeloJCapone <>
2020-02-07 16:03:54 -0500

    -Added a funny joke function

--- src/helloworldsam/HelloWorldSam.java
+++ src/helloworldsam/HelloWorldSam.java
@@ -1,11 +1,9 @@
-
 package helloworldsam;
-
 public class HelloWorldSam {
-
-    
     public static void main(String[] args) {
         
     }
-    
+    public static void somethingFunny(){
+        System.out.println("How many ROman emperors does it take to run an Empire for a year? 5! XD");
+    }
 }
